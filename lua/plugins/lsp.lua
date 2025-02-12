6accdf3bbc021f6405110bdda9b1b064df81be36
Abhimanyu Sharma <speedcuberabhi@gmail.com>
2025-02-12 22:08:14 +0530

    chore(mason-lspconfig): enable automatic installation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,6 +29,7 @@ return {
       local mason_lspconfig = require("mason-lspconfig")
 
       mason_lspconfig.setup({
+        automatic_installation = true,
         ensure_installed = vim.tbl_keys(opts.servers),
       })
 
